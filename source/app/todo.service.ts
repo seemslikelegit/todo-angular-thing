37cbab28a342cbdc96ff2885296f631443c7618e
Roman Husar <romaogcsm@gmail.com>
2018-05-23 13:14:05 +0300

    cleanup

--- source/app/todo.service.ts
+++ source/app/todo.service.ts
@@ -1,8 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { environment } from '../environments/environment';
-import { Observable, of } from 'rxjs';
-import { map } from 'rxjs/operators';
+import { Observable } from 'rxjs';
 import { Todo } from './todo'
 @Injectable({
   providedIn: 'root'
@@ -36,20 +35,4 @@ export class TodoService {
       .filter(todo => todo.id !== id);
     return this;
   }
-
-  // toggleTodo(id: number, values: Object = {}): Todo {
-  //   let todo = this.getTodoById(id);
-  //   if (!todo) {
-  //     return null;
-  //   }
-  //   Object.assign(todo, values);
-  //   return todo;
-  // }
-
-  // getTodoById(id: number): Todo {
-  //   return this.todos
-  //     .filter(todo => todo.id === id)
-  //     .pop();
-  // }
-
 }
